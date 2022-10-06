a3646ce14a0e75e7991c55264e957b219b6531ac
Dalton Carlile <dlcarlile93@hotmail.com>
2022-10-06 02:54:45 -0700

    commit tree 37b0bfed74b84303b1204dbb3a0e03fe480c0174

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(){
+    return['Antonia', 'Nuru']
+
+
+}
+
+const returnLastTwoDrivers = function(){
+    return['Amari','Mo']
+}
+
+const selectingDrivers = function(){
+    return returnFirstTwoDrivers,returnLastTwoDrivers
+}
